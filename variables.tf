a3943b204fa254c415be72c9332e8a2533afbf9f
malken21 <77374813+malken21@users.noreply.github.com>
2026-04-12 15:15:30 +0900

    sensitive を true に設定

--- variables.tf
+++ variables.tf
@@ -7,19 +7,23 @@ variable "cloudflare_api_token" {
 variable "cloudflare_account_id" {
   description = "Cloudflare Account ID"
   type        = string
+  sensitive   = true
 }
 
 variable "toropon_ip" {
   description = "Global IP address for toropon"
   type        = string
+  sensitive   = true
 }
 
 variable "argotunnel_cname_target" {
   description = "CNAME target for Cloudflare Tunnel"
   type        = string
+  sensitive   = true
 }
 
 variable "gh_pages_challenge_content" {
   description = "Content for GitHub Pages challenge TXT record"
   type        = string
+  sensitive   = true
 }
